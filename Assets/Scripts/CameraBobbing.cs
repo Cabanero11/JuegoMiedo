01e7d0d0eaa458863d14b52c76f42b6bbe723c2e
Cabanero11 <cabanero1111@gmail.com>
2024-11-12 08:39:27 +0100

    feat(add): Skybox añadida + Efecto Bobbing camara

---
+++ Assets/Scripts/CameraBobbing.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CameraBobbing : MonoBehaviour
+{
+    public Transform playerTransform;
+    public float bobbingSpeed = 0.18f;
+    public float bobbingAmount = 0.05f;
+    public float midpoint = 2.0f;
+
+    private float timer = 0.0f;
+    private float defaultYPos;
+
+    void Start()
+    {
+        defaultYPos = transform.localPosition.y;
+    }
+
+    void Update()
+    {
+        float waveslice = 0.0f;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
+        {
+            timer = 0.0f;
+        }
+        else
+        {
+            waveslice = Mathf.Sin(timer);
+            timer += bobbingSpeed;
+            if (timer > Mathf.PI * 2)
+            {
+                timer -= Mathf.PI * 2;
+            }
+        }
+
+        if (waveslice != 0)
+        {
+            float translateChange = waveslice * bobbingAmount;
+            float totalAxes = Mathf.Clamp(Mathf.Abs(horizontal) + Mathf.Abs(vertical), 0.0f, 1.0f);
+            translateChange *= totalAxes;
+            Vector3 newPos = transform.localPosition;
+            newPos.y = defaultYPos + translateChange;
+            transform.localPosition = newPos;
+        }
+        else
+        {
+            Vector3 newPos = transform.localPosition;
+            newPos.y = defaultYPos;
+            transform.localPosition = newPos;
+        }
+    }
+}
