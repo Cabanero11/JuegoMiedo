8a759cf261b8a08a2347ecd67db2ebb55bb64465
Cabanero11 <cabanero1111@gmail.com>
2024-11-21 13:14:56 +0100

    feat(add): Liquido Negro shader + Efecto halo en llaves

--- Assets/Scripts/CogerLlave.cs
+++ Assets/Scripts/CogerLlave.cs
@@ -6,8 +6,17 @@ public class CogerLlave : MonoBehaviour
 {
     [Header("Nombre de la llave")]
     public string keyID;
+    [Header("Particula Hijo")]
+    public GameObject particulaHalo;
     [Range(0f, 1f)]
     public float volume;
+    public float alturaAbajoHalo = 1f;
+
+    private void Awake()
+    {
+        // Poner el halo en la llave
+        particulaHalo.transform.position = new Vector3(transform.position.x, transform.position.y - alturaAbajoHalo, transform.position.z);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,11 +25,12 @@ public class CogerLlave : MonoBehaviour
             GameManager gameManager = FindObjectOfType<GameManager>();
             if (gameManager != null)
             {
-                GameSounds.Play("Key", 0.9f, 1.1f, 1f, 20f, transform.position);
+                GameSounds.Play("Key", volume, 1.1f, 1f, 20f, transform.position);
 
                 gameManager.RecogerLlave(keyID);
                 Debug.Log("llave: " + keyID + " recogida.");
                 Destroy(gameObject); // Destruye la llave al recogerla
+                Destroy(particulaHalo);
             }
             else
             {
