9344150ee6e53e75af5932e36bdfd21a529cb903
Cabanero11 <cabanero1111@gmail.com>
2024-11-22 09:30:19 +0100

    feat(add): Menu de Pausa
    Ta bugeado con el movimiento de la camara

--- Assets/Scripts/CameraBobbing.cs
+++ Assets/Scripts/CameraBobbing.cs
@@ -17,38 +17,42 @@ public class CameraBobbing : MonoBehaviour
 
     void Update()
     {
-        float waveslice = 0.0f;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if(!PauseMenuScript.isGamePausedStatic)
+        {
+            float waveslice = 0.0f;
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
 
-        if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
-        {
-            timer = 0.0f;
-        }
-        else
-        {
-            waveslice = Mathf.Sin(timer);
-            timer += bobbingSpeed;
-            if (timer > Mathf.PI * 2)
+            if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0)
             {
-                timer -= Mathf.PI * 2;
+                timer = 0.0f;
+            }
+            else
+            {
+                waveslice = Mathf.Sin(timer);
+                timer += bobbingSpeed;
+                if (timer > Mathf.PI * 2)
+                {
+                    timer -= Mathf.PI * 2;
+                }
+            }
+
+            if (waveslice != 0)
+            {
+                float translateChange = waveslice * bobbingAmount;
+                float totalAxes = Mathf.Clamp(Mathf.Abs(horizontal) + Mathf.Abs(vertical), 0.0f, 1.0f);
+                translateChange *= totalAxes;
+                Vector3 newPos = transform.localPosition;
+                newPos.y = defaultYPos + translateChange;
+                transform.localPosition = newPos;
+            }
+            else
+            {
+                Vector3 newPos = transform.localPosition;
+                newPos.y = defaultYPos;
+                transform.localPosition = newPos;
             }
         }
-
-        if (waveslice != 0)
-        {
-            float translateChange = waveslice * bobbingAmount;
-            float totalAxes = Mathf.Clamp(Mathf.Abs(horizontal) + Mathf.Abs(vertical), 0.0f, 1.0f);
-            translateChange *= totalAxes;
-            Vector3 newPos = transform.localPosition;
-            newPos.y = defaultYPos + translateChange;
-            transform.localPosition = newPos;
-        }
-        else
-        {
-            Vector3 newPos = transform.localPosition;
-            newPos.y = defaultYPos;
-            transform.localPosition = newPos;
-        }
+        
     }
 }
---
+++ Assets/Scripts/Menu/PauseMenuScript.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField] private bool isGamePaused = false;
+    public static bool isGamePausedStatic;
+
+    [Header("Referencias")]
+    public GameObject pauseMenuUI;
+    public GameObject reticula;
+
+    [Header("Opciones de Audio")]
+    public Slider volumeSlider; // Slider para el volumen
+    public TextMeshProUGUI volumenTexto;
+
+
+    private void Awake()
+    {
+        pauseMenuUI.SetActive(false); // No pausa, al inicio xd
+    }
+
+    private void Start()
+    {
+        // Configurar el slider de volumen
+        if (volumeSlider != null)
+        {
+            float savedVolume = PlayerPrefs.GetFloat("Volume", 1f); // Valor por defecto: 1 (máximo)
+            volumeSlider.value = savedVolume;
+            AudioListener.volume = savedVolume;
+
+            volumeSlider.onValueChanged.AddListener(delegate { SetVolume(volumeSlider.value); });
+        }
+    }
+
+
+    public void VolverAlMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        isGamePaused = false;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 1f;
+
+        //GameManager.Instance.gameState = GameState.menu;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Sali del juego :(");
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePauseMenu();
+        }
+    }
+
+
+
+    public void ContinueGame()
+    {
+        reticula.SetActive(true);
+        //pauseMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        isGamePaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        Time.timeScale = 1f;
+
+        //GameManager.Instance.gameState = GameState.level;
+    }
+
+    public void PauseGame()
+    {
+        reticula.SetActive(false);
+        pauseMenuUI.SetActive(true);
+
+        //pauseMenuUI.SetActive(true);
+
+        isGamePaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        Time.timeScale = 0f;
+
+    }
+
+
+
+    public void TogglePauseMenu()
+    {
+        if (isGamePaused)
+        {
+            ContinueGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+    // Función para establecer el volumen
+    public void SetVolume(float volume)
+    {
+        // Ajustar el volumen del AudioListener
+        AudioListener.volume = volume;
+
+        // Actualizar el texto del volumen
+        if (volumenTexto != null)
+        {
+            float mappedVolume = volume * 10f; // Escala el rango a 0-10
+            volumenTexto.text = Mathf.RoundToInt(mappedVolume).ToString();
+        }
+
+        // Guardar el volumen en PlayerPrefs
+        PlayerPrefs.SetFloat("Volume", volume);
+    }
+}
+
